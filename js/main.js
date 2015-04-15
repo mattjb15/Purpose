14ff7b2de7d591b946b3cdb461fae170c62cf7d6
mattjb15 <mattjb15@live.co.uk>
2015-04-15 18:32:52 +0100

    update
    added new pages and modified responsiveVoice integration

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
 /**
  * Main AngularJS Web Application
  */
-var app = angular.module('StrokerAir', [
+var app = angular.module('Purpose', [
   'ngRoute'
 ]);
 
@@ -27,3 +27,27 @@ app.config(['$routeProvider', function ($routeProvider) {
     .when("/custom", {templateUrl: "partials/custom.html", controller: "PageCtrl"})
     .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
+
+/**
+ * Controls the Blog
+ */
+app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
+  console.log("Blog Controller reporting for duty.");
+});
+
+/**
+ * Controls all other Pages
+ */
+app.controller('PageCtrl', function (/* $scope, $location, $http */) {
+  console.log("Page Controller reporting for duty.");
+
+  // Activates the Carousel
+  $('.carousel').carousel({
+    interval: 5000
+  });
+
+  // Activates Tooltips for Social Links
+  $('.tooltip-social').tooltip({
+    selector: "a[data-toggle=tooltip]"
+  })
+});
